325c07f8918290b98272fc99b7f277bc9b1dfc98
Vaibhav <vbodhane29@gmail.com>
2020-11-09 17:58:36 +0530

    Added HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list"""
+
+        an_apiview = [
+            'Vaibhav',
+            'Akshay',
+            'Apex',
+            'APEXDEAD'
+        ]
+
+        return Response({'message': 'Hello Wolrd', 'an_apiview': an_apiview})
